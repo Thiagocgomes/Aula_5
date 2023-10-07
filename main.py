324a51e8dac936e699bae8a990c6d5737ac34aca
Thiago de Carvalho <145858546+Thiagocgomes@users.noreply.github.com>
2023-10-07 12:39:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,127 @@
+# nome_da_cidade = 'Guarulhos'
+# comprimento = len(nome_da_cidade)
+# print(comprimento)
+
+# dia_da_semana = 'sábado'
+# print(len(dia_da_semana))
+
+# lista = [1,3,6,8]
+# print(len(lista))
+
+# x = 4.8
+# print(type(x))
+
+# z = '45'
+# n = float(z)
+# print(int(n))
+# print (n+5)
+
+# print()
+# cidade = 'Guarulhos'
+# print(cidade,'\n')
+# frag = list(cidade)
+#  print(frag)
+
+# tr = tuple(frag)
+# print(tr)
+
+# for a in range(1,11):
+#   print(a)
+
+# n = range(1,3)
+# print(n)
+
+# lista = [1,6,4,21,45]
+# soma = sum(lista)
+# print('A soma de tudo é: ',soma)
+
+# lista = [1,38,65,533]
+# maxi = max(lista)
+# print(maxi)
+
+# lista1 = ['z','a','r','g']
+# lista = {56,86,14,68,71}
+# organize = sorted(lista1)
+# organize1 = sorted(lista)
+# print(organize)
+# print(organize1)
+
+###Exercício 1: Escreva um programa que use a função range() para gerar os números pares de 2 a 20 e, em seguida, imprima cada número.
+
+# for i in range(2,22,2):
+#   print(i)  
+
+###Exercício 2: Escreva um programa que use a função range() para gerar os múltiplos de 5 em 5 a 50 e, em seguida, calcule e imprima a soma desses múltiplos.
+
+# for n in range(5,55,5):
+#   print(n)
+# lista = [5,10,15,20,25,30,35,40,45,50]
+# soma = sum(lista)
+# print ('A soma de todos os números é: ', soma)
+
+###Exercício 3: Escreva um programa que use a função type() para verificar o tipo de uma variável.
+
+# t = 5.08
+# print(type(t))
+
+###Exercício 4: Escreva um programa que use a função print() para imprimir uma mensagem de saudação personalizada, incluindo o nome do usuário.
+
+# palavra = 'Saudações,'
+# palavra1 = input('Digite seu nome: ')
+# print(palavra,palavra1)
+
+###Exercício 5: Escreva um programa que use a função range() para gerar os números ímpares de 1 a 10 e, em seguida, calcule e imprima a média desses números.
+
+# for h in range(1,11,2):
+#   print(h)
+# lista1 = [1,3,5,7,9]
+# soma1 = sum(lista1)
+# media = (soma1/5)
+# print('A soma desses números impares é: ',soma1)
+# print('A média desses números impares é: ',media)
+
+#######################################################################################
+
+# palavra = "Python"
+# for i in palavra:
+#   print(i)
+
+# lista = ['Maçã','Banana','Cereja','Uva']
+# for i in lista:
+#   print(i)
+
+### Dicionário
+
+# dados = {
+# 'Nome':'Thiago',
+# 'Idade': '23',
+# 'Endereco' : 'Rua 20'
+# }
+# for i1,i2 in dados.items():
+#   print(f'{i1} : {i2}')
+
+matriz = [[1,2,3],[4,5,6],[7,8,9]]
+for i in matriz:
+  for n in i:
+    print(n)
+     
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
